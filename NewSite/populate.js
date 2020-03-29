438581ca0f90028ee00a85f6cd801cbae117f7b1
Prakhar Garg <prakhar@live.ca>
2020-03-29 18:09:38 -0400

    changes

--- NewSite/populate.js
+++ NewSite/populate.js
@@ -5,6 +5,7 @@ var Education = {
 		"icon": "assets/icons/education/masters/logo.jpg",
 		"ref": "",
 		"priority": "3",
+		"hide": false,
 		"jobs" : [
 			{
 				"title": "Masters of Applied Science",
@@ -18,6 +19,7 @@ var Education = {
 		"icon": "assets/icons/education/bachlors/logo.jpg",
 		"ref": "",
 		"priority": "2",
+		"hide": false,
 		"jobs" : [
 			{
 				"title": "Mechatronics Engineering with Management and CO-OP",
@@ -31,6 +33,7 @@ var Education = {
 		"icon": "assets/icons/education/webdesign/logo.jpg",
 		"ref": "",
 		"priority": "1",
+		"hide": false,
 		"jobs" : [
 			{
 				"title": "Web Development",
@@ -44,6 +47,7 @@ var Education = {
 		"icon": "assets/icons/education/highschool/logo.jpg",
 		"ref": "",
 		"priority": "1",
+		"hide": false,
 		"jobs" : [
 			{
 				"title": "Highschool Diploma",
@@ -60,6 +64,7 @@ var workExperiences = {
 		"icon": "assets/icons/work/gdms/logo.jpg",
 		"ref": "",
 		"priority": "2",
+		"hide": false,
 		"jobs" : [
 			{
 				"title": "Software CO-OP Student",
@@ -73,6 +78,7 @@ var workExperiences = {
 		"icon": "assets/icons/work/mcmaster/logo.jpg",
 		"ref": "",
 		"priority": "1",
+		"hide": false,
 		"jobs" : [
 			{
 				"title": "Teaching Assistant",
@@ -96,6 +102,7 @@ var workExperiences = {
 		"icon": "assets/icons/work/thales/logo.jpg",
 		"ref": "",
 		"priority": "2",
+		"hide": false,
 		"jobs" : [
 			{
 				"title": "Software Developer CO-OP",
@@ -114,6 +121,7 @@ var workExperiences = {
 		"icon": "assets/icons/work/capreit/logo.jpg",
 		"ref": "",
 		"priority": "1",
+		"hide": false,
 		"jobs" : [
 			{
 				"title": "IT Summer Student",
@@ -132,6 +140,7 @@ var workExperiences = {
 		"icon": "assets/icons/work/ctw/logo.jpg",
 		"ref": "",
 		"priority": "1",
+		"hide": false,
 		"jobs" : [
 			{
 				"title": "Camp Instructor",
@@ -150,6 +159,7 @@ var workExperiences = {
 		"icon": "assets/icons/work/torontostar/logo.jpg",
 		"ref": "",
 		"priority": "1",
+		"hide": false,
 		"jobs" : [
 			{
 				"title": "Sales",
@@ -212,89 +222,111 @@ var Interests = {
 };
 
 var Projects = {
-	"distrist" : {
+	"district" : {
 		"company": "FIRST District Organisation",
 		"icon": "assets/icons/project/district/logo.jpg",
 		"ref": "",
-		"priority": "2"
+		"priority": "2",
+		"hide": false
+	},
+	"capstone" : {
+		"company": "Mechatronics Engineering Capstone",
+		"icon": "assets/icons/project/website/logo.jpg",
+		"ref": "",
+		"priority": "2",
+		"hide": false
 	},
 	"website" : {
 		"company": "Personal Website",
 		"icon": "assets/icons/project/website/logo.jpg",
 		"ref": "",
-		"priority": "1"
+		"priority": "1",
+		"hide": false
 	},
-	"quadcopter" : {
-		"company": "Quadcopter Design",
-		"icon": "assets/icons/project/quadcopter/logo.jpg",
-		"ref": "",
-		"priority": "1"
-	},
+	
 	"hyperloop" : {
 		"company": "McMaster Hyperloop",
 		"icon": "assets/icons/project/hyperloop/logo.jpg",
 		"ref": "",
-		"priority": "2"
+		"priority": "2",
+		"hide": false
 	},
 	"ecocar" : {
 		"company": "EcoCAR3",
 		"icon": "assets/icons/project/ecocar/logo.jpg",
 		"ref": "",
-		"priority": "2"
+		"priority": "2",
+		"hide": false
 	},
 	"formulahybrid" : {
 		"company": "Formula Hybrid",
 		"icon": "assets/icons/project/formulahybrid/logo.jpg",
 		"ref": "",
-		"priority": "1"
+		"priority": "1",
+		"hide": false
 	},
 	"strangers" : {
 		"company": "Strangers",
 		"icon": "assets/icons/project/strangers/logo.jpg",
 		"ref": "",
-		"priority": "1"
+		"priority": "1",
+		"hide": false
 	},
 	"quadsim" : {
 		"company": "QuadSim",
 		"icon": "assets/icons/project/quadsim/logo.jpg",
 		"ref": "",
-		"priority": "1"
+		"priority": "1",
+		"hide": false
 	},
 	"sumobot" : {
 		"company": "Sumobot",
 		"icon": "assets/icons/project/sumobot/logo.jpg",
 		"ref": "",
-		"priority": "2"
+		"priority": "2",
+		"hide": true
+	},
+	"quadcopter" : {
+		"company": "Quadcopter Design",
+		"icon": "assets/icons/project/quadcopter/logo.jpg",
+		"ref": "",
+		"priority": "1",
+		"hide": true
 	},
 	"class" : {
 		"company": "Class Projects",
 		"icon": "assets/icons/project/class/logo.jpg",
 		"ref": "",
-		"priority": "1"
+		"priority": "1",
+		"hide": true
 	},
 	"first" : {
 		"company": "FIRST Robotics",
 		"icon": "assets/icons/project/first/logo.jpg",
 		"ref": "",
-		"priority": "2"
+		"priority": "2",
+		"hide": true
 	},
 	"f1" : {
 		"company": "F1 in schools",
 		"icon": "assets/icons/project/f1/logo.jpg",
 		"ref": "",
-		"priority": "1"
+		"priority": "1",
+		"hide": true
 	},
 	"vex" : {
 		"company": "VEX Robotics",
 		"icon": "assets/icons/project/vex/logo.jpg",
 		"ref": "",
-		"priority": "1"
+		"priority": "1",
+		"hide": true
 	},
 	"skills" : {
 		"company": "Skills Robotics",
 		"icon": "assets/icons/project/skills/logo.jpg",
 		"ref": "",
-		"priority": "1"
+		"priority": "1",
+		"hide": true
 	},
 };
 
@@ -318,10 +350,17 @@ function pullProjects() {
 function pullExperience(experiences, target) {	
 	var container = document.createElement("div");
 	container.className = 'grid-container';
+	includeShowMoreButton = false;
 	for (company in experiences) {
 		var jobcontainer = document.createElement("a");
 		jobcontainer.href = domain + experiences[company].ref;
 		jobcontainer.className = "priority" + experiences[company].priority;
+		jobcontainer.className = jobcontainer.className  + " hidden" + experiences[company].hide;
+		jobcontainer.className = jobcontainer.className  + " block";
+ 
+		if (experiences[company].hide == true) {
+			includeShowMoreButton = true;
+		}
 		
 		var tag;
 		var content;
@@ -351,6 +390,27 @@ function pullExperience(experiences, target) {
 		container.appendChild(jobcontainer);
 
 
+	}
+	
+	if (includeShowMoreButton == true) {
+		showMoreContainer = document.createElement("a");
+		showMoreContainer.className = "showmorebutton"
+		showMoreContainer.id = target+"showmorebutton"
+		
+		var more = document.createElement("h2");
+		more.className = "showmore"
+		var moretext = document.createTextNode("Show More");
+		more.appendChild(moretext);
+		
+		var less = document.createElement("h2");
+		less.className = "showless"
+		var lesstext = document.createTextNode("Show Less");
+		less.appendChild(moretext);
+		
+		showMoreContainer.appendChild(more);
+		showMoreContainer.appendChild(less);
+		
+		container.appendChild(showMoreContainer);
 	}
 
 	
