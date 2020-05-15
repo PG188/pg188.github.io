0f6f8f7f49c72ea19e3aa07c20240feb6a7d4e47
Prakhar Garg <prakahr@live.ca>
2020-05-15 01:02:20 -0400

    using bootstrap

--- NewSite/HorizontalMenuBar/index.js
+++ NewSite/HorizontalMenuBar/index.js
@@ -1,4 +1,548 @@
+var sitemap = {
+	"Home" : {
+		"path": ""
+	},
+	"Education" : {
+		"path"				: "/#Eduation",
+
+		"masters" : {
+			"company": "McMaster University",
+			"icon": "assets/icons/education/masters/logo.jpg",
+			"path": "",
+			"priority": "3",
+			"hide": false,
+			"jobs" : [
+				{
+					"title": "Masters of Applied Science",
+					"start": "",
+					"stop": ""
+				}
+			]
+		},
+		"bachlors" : {
+			"company": "McMaster University",
+			"icon": "assets/icons/education/bachlors/logo.jpg",
+			"path": "",
+			"priority": "2",
+			"hide": false,
+			"jobs" : [
+				{
+					"title": "Mechatronics Engineering with Management and CO-OP",
+					"start": "",
+					"stop": ""
+				}
+			]
+		},
+		"webdesign" : {
+			"company": "Ryerson University",
+			"icon": "assets/icons/education/webdesign/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false,
+			"jobs" : [
+				{
+					"title": "Web Development",
+					"start": "",
+					"stop": ""
+				}
+			]
+		},
+		"highschool" : {
+			"company": "Woburn CI",
+			"icon": "assets/icons/education/highschool/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false,
+			"jobs" : [
+				{
+					"title": "Highschool Diploma",
+					"start": "",
+					"stop": ""
+				}
+			]
+		}
+
+
+	},
+	"Interests" : {
+		"path" 				: "/#Interest",
+		"gardening" : {
+			"company": "Gardening",
+			"icon": "assets/icons/interest/gardening/logo.jpg",
+			"path": "",
+			"priority": "2",
+			"hide": false
+		},
+		"cooking" : {
+			"company": "Cooking",
+			"icon": "assets/icons/interest/cooking/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false
+		},
+		"axes" : {
+			"company": "Axe Throwing",
+			"icon": "assets/icons/interest/axes/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false
+		},
+		"3dprint" : {
+			"company": "3D Printing",
+			"icon": "assets/icons/interest/3dprint/logo.jpg",
+			"path": "",
+			"priority": "2",
+			"hide": false
+		},
+		"photography" : {
+			"company": "Photography",
+			"icon": "assets/icons/interest/photography/logo.jpg",
+			"path": "/Interests/Photography/index.html",
+			"priority": "2",
+			"hide": false
+		},
+		"robotics" : {
+			"company": "Robotics",
+			"icon": "assets/icons/interest/robotics/logo.jpg",
+			"path": "/Interests/Robots/index.html",
+			"priority": "1",
+			"hide": false
+		},
+		"travel" : {
+			"company": "Travelling",
+			"icon": "assets/icons/interest/travel/logo.jpg",
+			"path": "/Interests/Travelling/index.html",
+			"priority": "1",
+			"hide": false
+		},
+		"origami" : {
+			"company": "Origami",
+			"icon": "assets/icons/interest/origami/logo.jpg",
+			"path": "/Interests/Origami/index.html",
+			"priority": "1",
+			"hide": false
+		}
+	},	
+	"Employment" : {	
+		"path" 				: "/#Work",
+		"gdms" : {
+			"company": "General Dynamics Mission Systems",
+			"icon": "assets/icons/work/gdms/logo.jpg",
+			"path": "",
+			"priority": "2",
+			"hide": false,
+			"jobs" : [
+				{
+					"title": "Software CO-OP Student",
+					"start": "",
+					"stop": ""
+				}
+			]
+		},
+		"mcmaster" : {
+			"company": "McMaster University",
+			"icon": "assets/icons/work/mcmaster/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false,
+			"jobs" : [
+				{
+					"title": "Teaching Assistant",
+					"start": "",
+					"stop": ""
+				},
+				{
+					"title": "Research Assistant",
+					"start": "",
+					"stop": ""
+				},
+				{
+					"title": "Special Projects Assistant",
+					"start": "",
+					"stop": ""
+				}
+			]
+		},
+		"thales" : {
+			"company": "THALES Transportation Canada",
+			"icon": "assets/icons/work/thales/logo.jpg",
+			"path": "",
+			"priority": "2",
+			"hide": false,
+			"jobs" : [
+				{
+					"title": "Software Developer CO-OP",
+					"start": "",
+					"stop": ""
+				},
+				{
+					"title": "Junior Software Developer CO-OP",
+					"start": "",
+					"stop": ""
+				}
+			]
+		},
+		"capreit" : {
+			"company": "CAPREIT",
+			"icon": "assets/icons/work/capreit/logo.jpg",
+			"path": "/Work/CAPREIT/index.html",
+			"priority": "1",
+			"hide": false,
+			"jobs" : [
+				{
+					"title": "IT Summer Student",
+					"start": "",
+					"stop": ""
+				},
+				{
+					"title": "IT Summer Student",
+					"start": "",
+					"stop": ""
+				}
+			]
+		},
+		"ctw" : {
+			"company": "Children's Technology Workshop",
+			"icon": "assets/icons/work/ctw/logo.jpg",
+			"path": "/Work/CTWorkshop/index.html",
+			"priority": "1",
+			"hide": false,
+			"jobs" : [
+				{
+					"title": "Camp Instructor",
+					"start": "",
+					"stop": ""
+				},
+				{
+					"title": "Camp Instructor",
+					"start": "",
+					"stop": ""
+				}
+			]
+		},
+		"torontostar" : {
+			"company": "The Toronto Star",
+			"icon": "assets/icons/work/torontostar/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false,
+			"jobs" : [
+				{
+					"title": "Sales",
+					"start": "",
+					"stop": ""
+				}
+			]
+		}
+	},	
+	"Projects" : {	
+		"path" 			: "/#Project",
+
+		"printedcnc" : {
+			"company": "Printed CNC",
+			"icon": "assets/icons/project/printedcnc/logo.jpg",
+			"ref": "",
+			"priority": "2",
+			"hide": false
+		},
+		"capstone" : {
+			"company": "Mechatronics Engineering Capstone",
+			"icon": "assets/icons/project/website/logo.jpg",
+			"path": "",
+			"priority": "2",
+			"hide": false
+		},
+		"website" : {
+			"company": "Personal Website",
+			"icon": "assets/icons/project/website/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false
+		},
+		
+		"hyperloop" : {
+			"company": "McMaster Hyperloop",
+			"icon": "assets/icons/project/hyperloop/logo.jpg",
+			"path": "",
+			"priority": "2",
+			"hide": false
+		},
+		"ecocar" : {
+			"company": "EcoCAR3",
+			"icon": "assets/icons/project/ecocar/logo.jpg",
+			"path": "/Projects/EcoCAR/index.html",
+			"priority": "2",
+			"hide": false
+		},
+		"formulahybrid" : {
+			"company": "Formula Hybrid",
+			"icon": "assets/icons/project/formulahybrid/logo.jpg",
+			"path": "/Projects/FormulaHybrid/index.html",
+			"priority": "1",
+			"hide": false
+		},
+		"strangers" : {
+			"company": "Strangers",
+			"icon": "assets/icons/project/strangers/logo.jpg",
+			"path": "/Projects/Strangers/index.html",
+			"priority": "1",
+			"hide": false
+		},
+		"quadsim" : {
+			"company": "QuadSim",
+			"icon": "assets/icons/project/quadsim/logo.jpg",
+			"path": "/Projects/QuadSim/index.html",
+			"priority": "1",
+			"hide": false
+		},
+		"sumobot" : {
+			"company": "Sumobot",
+			"icon": "assets/icons/project/sumobot/logo.jpg",
+			"path": "/Projects/SumoBot/index.html",
+			"priority": "2",
+			"hide": true
+		},
+		"quadcopter" : {
+			"company": "Quadcopter Design",
+			"icon": "assets/icons/project/quadcopter/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": true
+		},
+		"class" : {
+			"company": "Class Projects",
+			"icon": "assets/icons/project/class/logo.jpg",
+			"path": "/Projects/Class/index.html",
+			"priority": "1",
+			"hide": true
+		},
+		"first" : {
+			"company": "FIRST Robotics",
+			"icon": "assets/icons/project/first/logo.jpg",
+			"path": "/Projects/FIRST/index.html",
+			"priority": "2",
+			"hide": true
+		},
+		"f1" : {
+			"company": "F1 in schools",
+			"icon": "assets/icons/project/f1/logo.jpg",
+			"path": "/Projects/F1/index.html",
+			"priority": "1",
+			"hide": true
+		},
+		"vex" : {
+			"company": "VEX Robotics",
+			"icon": "assets/icons/project/vex/logo.jpg",
+			"path": "/Projects/VEX/index.html",
+			"priority": "1",
+			"hide": true
+		},
+		"skills" : {
+			"company": "Skills Robotics",
+			"icon": "assets/icons/project/skills/logo.jpg",
+			"path": "/Projects/Skills/index.html",
+			"priority": "1",
+			"hide": true
+		}
+	},
+	"Community"	: {
+		"path" 			: "/#Community",
+		"district" : {
+			"company": "FIRST District Organisation",
+			"icon": "assets/icons/community/district/logo.jpg",
+			"path": "",
+			"priority": "2",
+			"hide": false
+		},
+		"macfirst" : {
+			"company": "Robotics Alumni Group",
+			"icon": "assets/icons/community/macfirst/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false
+		},
+		"volunteering" : {
+			"company": "Robotics Volunteering",
+			"icon": "assets/icons/community/volunteering/logo.jpg",
+			"path": "",
+			"priority": "2",
+			"hide": false
+		},
+		"mentoring" : {
+			"company": "Mentoring",
+			"icon": "assets/icons/community/mentoring/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false
+		},
+	},
+	"Contact" : {
+		"path" 			: "/#Contact",
+		"linkedin" : {
+			"company": "LinkedIn",
+			"icon": "assets/icons/community/district/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false
+		},
+		"github" : {
+			"company": "GitHub",
+			"icon": "assets/icons/community/macfirst/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false
+		},
+		"challengepost" : {
+			"company": "Challenge Post",
+			"icon": "assets/icons/community/volunteering/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false
+		},
+		"gmail" : {
+			"company": "Gmail",
+			"icon": "assets/icons/community/mentoring/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false
+		},
+		"hotmail" : {
+			"company": "Hotmail",
+			"icon": "assets/icons/community/mentoring/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false
+		},
+		"twitter" : {
+			"company": "Twitter",
+			"icon": "assets/icons/community/mentoring/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false
+		},
+		"flickr" : {
+			"company": "Flickr",
+			"icon": "assets/icons/community/mentoring/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false
+		},
+		"instagram" : {
+			"company": "Instagram",
+			"icon": "assets/icons/community/mentoring/logo.jpg",
+			"path": "",
+			"priority": "1",
+			"hide": false
+		},
+	}
+};
+
+
+var domain = 'http://www.prakhargarg.me';
+
 function addMenubar() {
+
+var nav
+var link
+var button
+var span
+
+nav = document.createElement("nav");
+nav.className = "navbar navbar-expand-lg navbar-dark bg-primary";
+link = document.createElement("a"); 
+link.className = "navbar-brand";
+link.href = "http://www.prakhargarg.me/" +  "#";
+link.innerHTML = "Prakhar Garg"
+
+button = document.createElement("button"); 
+button.className = "navbar-toggler";
+button.setAttribute("type","button");
+button.setAttribute("data-toggle","collapse");
+button.setAttribute("data-target","#navbarSupportedContent");
+button.setAttribute("aria-controls","navbarSupportedContent");
+button.setAttribute("aria-expanded","false");
+button.setAttribute("aria-label","Toggle navigation");
+
+span = document.createElement("span"); 
+span.className = "navbar-toggler-icon";
+
+button.appendChild(span);
+
+nav.appendChild(link);
+nav.appendChild(button);
+
+delete link
+delete button
+delete span
+
+var div
+var ul
+var li
+var a
+var textnode
+
+div = document.createElement("div");
+div.className = "collapse navbar-collapse";
+div.id = "navbarSupportedContent";
+
+ul = document.createElement("ul");
+ul.className = "navbar-nav mr-auto";
+
+for (mainitem in sitemap) {
+	li = document.createElement("li");
+	li.className = "nav-item";
+	a = document.createElement("a");
+	a.className = "nav-link";
+	a.href = "http://www.prakhargarg.me/" +  sitemap[mainitem].path;
+	textnode = document.createTextNode(mainitem);
+	a.appendChild(textnode);
+	var dropdown = false;
+	for (subitem in sitemap[mainitem]) {
+		var subdiv = document.createElement("div");
+		subdiv.className = "dropdown-menu";
+		subdiv.setAttribute("aria-labelledby","navbarDropdown");
+		if (typeof sitemap[mainitem][subitem].path === 'string'){
+			dropdown = true;
+			var suba = document.createElement("a");
+			suba.className = "dropdown-item";
+			suba.href = "http://www.prakhargarg.me/" +  sitemap[mainitem][subitem].path;
+			textnode = document.createTextNode(subitem);
+			suba.appendChild(textnode);
+			subdiv.appendChild(suba);
+		}
+	}
+
+	if (dropdown == true){
+		li.className = li.className + " dropdown";
+		a.className = a.className + " dropdown-toggle";
+		a.id="navbarDropdown" 
+		a.setAttribute("role","button");
+		a.setAttribute("data-toggle","dropdown");
+		a.setAttribute("aria-haspopup","true");
+		a.setAttribute("aria-expanded","false");
+	}
+
+
+	
+	li.appendChild(a);
+
+
+	if (dropdown == true){
+		li.appendChild(subdiv);
+	}
+
+
+	ul.appendChild(li);
+}
+
+div.appendChild(ul);
+nav.appendChild(div);
+
+document.body.prepend(nav, document.body.firstChild);
+
+}
+
+function addMenubar_ignore() {
 	var references = {
 		'Home' : '',
 		'Interests' : {
@@ -60,7 +604,7 @@ function addMenu(references,level) {
 				var a = document.createElement("a");
 				var node = document.createTextNode(item);
 				a.appendChild(node);
-				a.href = domain + references[item];
+				a.href = "http://www.prakhargarg.me/" +  domain + references[item];
 				li.appendChild(a);
 				ul.appendChild(li);
 			}
@@ -69,7 +613,7 @@ function addMenu(references,level) {
 				li.className = 'dropdown';
 				var a = document.createElement("a");
 				a.className = 'subMenu';
-				a.href = domain + references[item]['path'];
+				a.href = "http://www.prakhargarg.me/" +  domain + references[item]['path'];
 				var node = document.createTextNode(item);
 				a.appendChild(node);
 				li.appendChild(a);
