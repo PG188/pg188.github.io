8adbd0e4d013711b5020d270314239adb0e60cfb
Prakhar Garg <prakahr@live.ca>
2021-02-19 15:00:12 -0500

    changes for photogallery

---
+++ assets/data/photoGallery.js
@@ -0,0 +1,26 @@
+var photoGallery = [
+	{
+		'name' = 'test',
+		'address' = '',
+		'discription' = '',
+		'emphasis' = '',
+		'orientation' = 'square',
+		'tag' = ''
+	},
+	{
+		'name' = 'test',
+		'address' = '',
+		'discription' = '',
+		'emphasis' = '',
+		'orientation' = 'landscape',
+		'tag' = ''
+	},
+	{
+		'name' = 'test',
+		'address' = '',
+		'discription' = '',
+		'emphasis' = '',
+		'orientation' = 'portrait',
+		'tag' = ''
+	}
+]
