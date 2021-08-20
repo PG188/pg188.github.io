48aac0cf0bf461329b6f555cbb4324d878ee4f99
pg188 <prakhar@live.ca>
2021-08-20 01:39:35 -0400

    gardening init

---
+++ assets/data/plants.js
@@ -0,0 +1,146 @@
+var photoGallery = [
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (1).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (2).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (3).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (4).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (5).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'portrait',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (6).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (7).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (8).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (9).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (10).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (11).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (12).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (13).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'portrait',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (14).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (15).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'portrait',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (16).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'portrait',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (17).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'portrait',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/plants (18).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'square',
+		'tag' : ''
+	}
+]
