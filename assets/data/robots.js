a35bafa1e3e4b317e3190d002e4e9e84b6b29e39
pg188 <prakhar@live.ca>
2021-08-20 01:25:39 -0400

    robots init

---
+++ assets/data/robots.js
@@ -0,0 +1,226 @@
+var photoGallery = [
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (1).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (2).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (3).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (4).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (5).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (6).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (7).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (8).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (9).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (10).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (11).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (12).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (13).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (14).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (15).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'portrait',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (16).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (17).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'portrait',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (18).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (19).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'portrait',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (20).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (21).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (22).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (23).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (24).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (25).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (26).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (27).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/robots (28).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	}
+]
