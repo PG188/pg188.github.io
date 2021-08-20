6194ee38ec7782fc1d40a8332154773b08edc774
pg188 <prakhar@live.ca>
2021-08-20 00:59:31 -0400

    cooking init

---
+++ assets/data/cooking.js
@@ -0,0 +1,234 @@
+var photoGallery = [
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (1).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (2).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (3).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (4).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (5).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (6).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (7).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (8).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (9).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (10).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (11).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (12).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (13).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (14).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (15).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (16).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (17).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (18).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (19).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (20).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (21).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (22).jpg',
+		'discription' : '',
+		'priority' : '2',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (23).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (24).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (25).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (26).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'landscape',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (27).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (28).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+	{
+		'name' : 'test',
+		'path' : '/assets/photos/cooking (29).jpg',
+		'discription' : '',
+		'priority' : '1',
+		'orientation' : 'square',
+		'tag' : ''
+	},
+]
